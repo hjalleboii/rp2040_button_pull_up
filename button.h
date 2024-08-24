f3626d9d253e37e44f6fb20a219eeb1e7ffc7cdb
Hjalmar <test@nagot.com>
2024-08-24 18:04:34 +0200

    fixed ifdef __cplusplus

--- button.h
+++ button.h
@@ -1,11 +1,14 @@
 #include "pico/stdlib.h"
 #include "stdint.h"
 
+#ifdef __cplusplus
 extern "C"{
-
+#endif
 typedef uint8_t RP2040_Button;
 
 RP2040_Button RP2040_Button_init(uint8_t);
 bool RP2040_Button_get(RP2040_Button);
 
+#ifdef __cplusplus
 }
+#endif
