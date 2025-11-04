7f6362856046f47e72efdec7cad562e8a58276d0
Hjalle <hjalle.figment069@passinbox.com>
2025-11-04 15:33:39 +0100

    Added timeout function because of button bouncing

--- button.c
+++ button.c
@@ -35,3 +35,32 @@ bool RP2040_Button_Get_Once(RP2040_Button *button){
     
 
 }
+
+
+bool RP2040_Button_Get_Once_timeout(RP2040_Button *button, uint32_t * current_timeout_us, uint32_t dt_us, uint32_t set_timeout_us){
+
+    uint8_t pressed = (*button)&(0x80);
+
+    bool v = false;
+
+    uint8_t pin = (*button) & 0x3F;
+    if(!gpio_get(pin)){
+        if((!pressed) && (*current_timeout_us == 0)){
+            *button = pin | 0x80;
+            *current_timeout_us = set_timeout_us;
+            v = true;
+        }
+
+    }else{
+        if (*current_timeout_us > dt_us) {
+            *current_timeout_us -= dt_us;     // Safe subtraction — no underflow
+        } else {
+            *current_timeout_us = 0;       // Clamp to zero when dt >= timeout
+        }
+        *button = pin;
+    }
+
+    return v;
+    
+
+}
--- button.h
+++ button.h
@@ -10,6 +10,8 @@ RP2040_Button RP2040_Button_init(uint8_t);
 bool RP2040_Button_get(RP2040_Button);
 bool RP2040_Button_Get_Once(RP2040_Button *button);
 
+bool RP2040_Button_Get_Once_timeout(RP2040_Button *button, uint32_t *current_timeout_us, uint32_t dt_us, uint32_t set_timeout_us);
+
 #ifdef __cplusplus
 }
 #endif
